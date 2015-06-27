547d107c329398a28ec3c828eb75d4bc0bef9c6c
Jonathan Heilmann <mail@jonathan-heilmann.de>
2015-06-27 13:50:26 +0200

    [BUG] Not compatibe to tx_news 3.2.0

--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Magnific Popup only');
-
 ## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
 
 /**
